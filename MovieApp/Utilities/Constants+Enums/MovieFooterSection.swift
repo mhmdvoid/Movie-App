02996efff0f71c04e74ae488b3f28fcfb006408c
MohaTech10 <moody54324@gmail.com>
2020-12-25 23:50:27 +0300

    Add shimmer view

---
+++ MovieApp/Utilities/Constants+Enums/MovieFooterSection.swift
@@ -0,0 +1,9 @@
+//
+//  MovieFooterSection.swift
+//  MovieApp
+//
+//  Created by Mohammed mohsen on 18/12/2020.
+//  Copyright © 2020 Mohammed mohsen. All rights reserved.
+//
+
+import Foundation
